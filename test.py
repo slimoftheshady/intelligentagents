d8ed2bbe915e51e3a4a6e164580d97499039df3a
_slim_ <89131181+slimoftheshady@users.noreply.github.com>
2026-09-16 10:09:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from game import run_one_game
 from collections import defaultdict
 from agent_baselines import StaticAgent, RandomAgent, GreedyAgent, AttitudeAgent
-from agent_groupnumber import StudentAgent
+from agent_57 import StudentAgent
 
 # This file provides examples for you to test the performance of your agents. The testing code may be different during the marking.
 
