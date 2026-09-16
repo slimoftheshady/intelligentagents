b2da53ec3b4293c14df7bebf41e64d0c569adfe3
_slim_ <89131181+slimoftheshady@users.noreply.github.com>
2026-09-16 10:09:20 +0800

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 from game import run_one_game
 from agent_baselines import StaticAgent, RandomAgent, GreedyAgent, AttitudeAgent
-from agent_groupnumber import StudentAgent
+from agent_57 import StudentAgent
 
 # This file provides an example to simulate one game and export the game process for visualization.
 
